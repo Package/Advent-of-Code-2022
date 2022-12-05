a47000cfbd460fad56a5c91255e6f1a9b4e1b28c
Package <ryan.dean1000@outlook.com>
2022-12-05 10:19:10 +0000

    Solve Day 05 (Part 1)

---
+++ AdventOfCode.Tests/Solutions/Day05Tests.cs
@@ -0,0 +1,88 @@
+using AdventOfCode.Solutions;
+
+namespace AdventOfCode.Tests.Solutions
+{
+    public class Day05Tests
+    {
+        [Test]
+        public void ParseInstructions_ReturnsTheListOfCrateInstructions()
+        {
+            // Arrange
+            var solution = new Day05();
+
+            // Act
+            var instructions = solution.ParseInstructions(@"Input/Day05_Example.txt");
+
+            // Assert
+            Assert.That(instructions.Count, Is.EqualTo(4));
+            Assert.Multiple(() =>
+            {
+                Assert.That(instructions[0].FromCrate, Is.EqualTo(2));
+                Assert.That(instructions[0].ToCreate, Is.EqualTo(1));
+                Assert.That(instructions[0].AmountToMove, Is.EqualTo(1));
+                
+                Assert.That(instructions[1].FromCrate, Is.EqualTo(1));
+                Assert.That(instructions[1].ToCreate, Is.EqualTo(3));
+                Assert.That(instructions[1].AmountToMove, Is.EqualTo(3));
+                
+                Assert.That(instructions[2].FromCrate, Is.EqualTo(2));
+                Assert.That(instructions[2].ToCreate, Is.EqualTo(1));
+                Assert.That(instructions[2].AmountToMove, Is.EqualTo(2));
+                
+                Assert.That(instructions[3].FromCrate, Is.EqualTo(1));
+                Assert.That(instructions[3].ToCreate, Is.EqualTo(2));
+                Assert.That(instructions[3].AmountToMove, Is.EqualTo(1));                
+            });
+        }
+        
+        [Test]
+        public void ParseInitialCrates_ReturnsTheQueueOfInitialStates()
+        {
+            // Arrange
+            var solution = new Day05();
+
+            // Act
+            var cratesMap = solution.ParseInitialCrates(@"Input/Day05_Example.txt");
+
+            // Assert
+            Assert.That(cratesMap.Keys.Count, Is.EqualTo(3));
+            Assert.Multiple(() =>
+            {
+                Assert.True(cratesMap[1].Contains("N"));
+                Assert.True(cratesMap[1].Contains("Z"));
+                
+                Assert.True(cratesMap[2].Contains("D"));
+                Assert.True(cratesMap[2].Contains("C"));
+                Assert.True(cratesMap[2].Contains("M"));
+                
+                Assert.True(cratesMap[3].Contains("P"));
+            });
+        }        
+        
+        [Test]
+        public void PartOne_ReturnsCorrectAnswer()
+        {
+            // Arrange
+            var solution = new Day05();
+
+            // Act
+            var result = solution.PartOne();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("ZSQVCCJLL"));
+        }
+
+        [Test]
+        public void PartTwo_ReturnsCorrectAnswer()
+        {
+            // Arrange
+            var solution = new Day05();
+
+            // Act
+            var result = solution.PartTwo();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/Day05.cs
@@ -0,0 +1,90 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solutions
+{
+    public class Day05 : ISolution<string>
+    {
+        private readonly Regex _charRegex = new(@"[\[(A-Z)\]]+", RegexOptions.Compiled);
+        private readonly Regex _moveRegex = new(@"move ([\d]+) from ([\d]+) to ([\d]+)", RegexOptions.Compiled);
+
+        public string PartOne()
+        {
+            var crateMap = ParseInitialCrates(@"Input/Day05_Real.txt");
+            var moveInstructions = ParseInstructions(@"Input/Day05_Real.txt");
+
+            foreach (var instruction in moveInstructions)
+            {
+                var movingFromCrate = crateMap[instruction.FromCrate];
+                var movingToCrate = crateMap[instruction.ToCreate];
+                
+                for (var amount = 0; amount < instruction.AmountToMove; amount++)
+                {
+                    movingToCrate.Push(movingFromCrate.Pop());
+                }
+            }
+
+            var firstItemsInEachStack = string.Empty;
+            
+            var sortedCrateMap = crateMap.OrderBy(cm => cm.Key).ToList();
+            sortedCrateMap.ForEach(stack => firstItemsInEachStack += stack.Value.Peek());
+
+            return firstItemsInEachStack;
+        }
+        
+        public string PartTwo()
+        {
+            return string.Empty;
+        }
+        
+        public List<CrateInstruction> ParseInstructions(string inputFile)
+        {
+            var input = File.ReadAllLines(inputFile);
+            var crateInstructions = new List<CrateInstruction>();
+
+            foreach (var line in input)
+            {
+                var moveMatch = _moveRegex.Match(line);
+                if (!moveMatch.Success)
+                    continue;
+
+                var instruction = new CrateInstruction(int.Parse(moveMatch.Groups[1].Value),
+                    int.Parse(moveMatch.Groups[2].Value), int.Parse(moveMatch.Groups[3].Value));
+                crateInstructions.Add(instruction);
+            }
+
+            return crateInstructions;
+        }
+        
+        public Dictionary<int, Stack<string>> ParseInitialCrates(string inputFile)
+        {
+            var input = File.ReadAllLines(inputFile);
+            var crateMap = new Dictionary<int, Stack<string>>();
+
+            foreach (var line in input)
+            {
+                var crateMatch = _charRegex.Match(line);
+                while (crateMatch.Success)
+                {
+                    var stackIndex = crateMatch.Index / 4 + 1;
+                    var character = crateMatch.Groups[0].Value.Replace("[", "").Replace("]", "");
+
+                    if (!crateMap.ContainsKey(stackIndex))
+                        crateMap[stackIndex] = new Stack<string>();
+
+                    crateMap[stackIndex].Push(character);
+                    crateMatch = crateMatch.NextMatch();
+                }
+            }
+
+            foreach (var crateKey in crateMap.Keys)
+            {
+                // Hacky trick to reverse the stacks
+                crateMap[crateKey] = new Stack<string>(crateMap[crateKey]);
+            }
+
+            return crateMap;
+        }
+
+        public record CrateInstruction(int AmountToMove, int FromCrate, int ToCreate);
+    }
+}
